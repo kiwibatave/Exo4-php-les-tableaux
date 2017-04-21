ba84f6878ec122c21cc602783fe0a951ff4eb8b6
kiwibatave <b_cuzon@yahoo.fr>
2017-04-21 11:24:09 +0200

    exos terminés

--- index.php
+++ index.php
@@ -12,19 +12,36 @@
     $mois [7] = "Août";
     echo $mois [7]. "<br />";
 
-    $departements = array ('Aisne' => '02',
-                           'Nord' => '59',
-                           'Oise' => '60',
-                           'Pas de Calais' => '62',
-                           'Somme' => '80',);
-    echo $departements ['Nord']. "<br />";
+    $departements = array('02' => 'Aisne',
+                           '59' => 'Nord',
+                           '60' => 'Oise',
+                           '62' => 'Pas de Calais',
+                           '80' => 'Somme',);
+    echo $departements['59']. "<br />";
+
+    $departements['51'] = "Marne";
+    // array_push($departements, '51');
+    // echo $departements['51']. "<br />";
+    // var_dump ($departements);
+?>
+</p>
+
+<p><?php
+  foreach ($mois as $mois) {
+    echo "<li>$mois</li>";
+  }
+  ?></p>
+<p><?php
+     echo implode("<br>", $departements);
+     ?>
+</p>
+<p><?php
+    foreach ($departements as $index => $valeur) {
+      echo ("<br>". 'Le département ' .$valeur. ' a le numéro ' .$index);
+     }
 
 
-
-
-
-
-
-   ?></p>
+   ?>
+ </p>
 </body>
 </html>
